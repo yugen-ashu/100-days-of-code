68d1bac6569ae2a0b02197e78867368aca149b99
Bhardwaj <yugen.ashu@gmail.com>
2026-04-08 01:26:26 +0545

    simple program for using format specifier

---
+++ day-02/format_practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int age = 20;
+    float height = 5.0;
+    char grade = 'A';
+    char name[] = "John";
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Passing Percentage: 40%%\n");
+
+
+    return 0;
+} 
